b1f2b8145e27f841efe624a6c9edc85f8351dceb
martin-stefan <55285748+martin-stefan@users.noreply.github.com>
2020-07-30 16:54:20 -0400

    morning 3

--- complete-javascript-course-master/5-advanced-JS/starter/script.js
+++ complete-javascript-course-master/5-advanced-JS/starter/script.js
@@ -1,20 +1,41 @@
 // Function constructor
 
-var john = {
-  name: 'john',
-  yearOfBirth: 1990,
-  job: 'teacher'
-};
+// var john = {
+//   name: 'john',
+//   yearOfBirth: 1990,
+//   job: 'teacher'
+// };
 
-var Person = function(name, yearOfBirth, job) {
-  this.name = name;
-  this.yearOfBirth = name;
-  this.job = job;
-}
+// var Person = function(name, yearOfBirth, job) {
+//   this.name = name;
+//   this.yearOfBirth = name;
+//   this.job = job;
+// }
 
-// creating a prototype function
-Person.prototype.calculateAge = function() {
-  console.log(this.yearOfBirth);
-}
+// // creating a prototype function
+// Person.prototype.calculateAge = function() {
+//   console.log(this.yearOfBirth);
+// }
 
-var jack = new Person('jack', 1999, 'professor');
+// var jack = new Person('jack', 1999, 'professor');
+
+// Creating an object
+
+
+// var personProto = {
+//   calculateAge: function() {
+//     console.log(2020 - this.yearOfBirth);
+//   }
+// };
+
+// var john = Object.create(personProto);
+// john.name = 'name';
+// john.yearOfBirth = 1990;
+// john.job = 'teacher';
+
+
+// var jane = Object.create(personProto, {
+//   name: {value: 'Jane'},
+//   yearOfBirth: {value: 1992},
+//   job: { value: 'designer'}
+// })
