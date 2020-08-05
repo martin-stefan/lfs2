9ea4cab915c323c611c5fce6cebb6c0098a1e070
martin-stefan <55285748+martin-stefan@users.noreply.github.com>
2020-08-05 19:08:52 -0400

    stuff

--- complete-javascript-course-master/7-ES6/starter/script.js
+++ complete-javascript-course-master/7-ES6/starter/script.js
@@ -55,26 +55,98 @@
 
 // es6 arrow functions
 
-const years = [1990, 1999, 1989];
+// const years = [1990, 1999, 1989];
+
+// // es5
+// // var ages = years.map(function(el) {
+// //   return 2016 - el;
+// // });
+
+
+// // es6
+// const ages = years.map(el => 2016 - el);
+// console.log(ages);
+
+
+// let ages2 = years.map((el, index) => `Age of ${index + 1}: ${2016 - el}`);
+// console.log(ages2);
+
+// ages2 = years.map((el, index) => {
+//   const now = new Date().getFullYear();
+//   const age = now - el;
+//   return `Age of ${index + 1}: ${age}`
+// })
+
+// console.log(ages2)
+
+// arrow functions share the surrounding this keyword
+
 
 // es5
-// var ages = years.map(function(el) {
-//   return 2016 - el;
-// });
+// does not work how we want it to
+// to make it work, save this to a variable
+// var box = {
+//   color: 'green',
+//   position: 1,
+//   clickMe: function() {
+//     document.querySelector('.green').addEventListener('click', function() {
+//       var str = "this box number is " + this.position + 'and it is' + this.color;
+//       alert(str)
+//     });
+//   }
+// }
+// box.clickMe();
 
 
 // es6
-const ages = years.map(el => 2016 - el);
-console.log(ages);
+// const box = {
+//   color: 'green',
+//   position: 1,
+//   clickMe: function() {
+//     document.querySelector('.green').addEventListener('click', () => {
+//       var str = "this box number is " + this.position + ' and it is' + this.color;
+//       alert(str)
+//     });
+//   }
+// }
+// box.clickMe();
 
 
-let ages2 = years.map((el, index) => `Age of ${index + 1}: ${2016 - el}`);
-console.log(ages2);
+// es5
+// also does not work
+// to make it work, bind the call function
+// function Person(name) {
+//   this.name = name;
+// }
 
-ages2 = years.map((el, index) => {
-  const now = new Date().getFullYear();
-  const age = now - el;
-  return `Age of ${index + 1}: ${age}`
-})
+// Person.prototype.myFriends = function(friends) {
+//   var arr = friends.map(function(el) {
+//     return this.name + ' is friends with ' + el;
+//   });
+//   console.log(arr);
+// }
 
-console.log(ages2)
+// var friends = ['bob', 'jane', 'mark'];
+// new Person('John').myFriends(friends);
+
+// es6
+// function Person(name) {
+//     this.name = name;
+// }
+
+// Person.prototype.myFriends = function(friends) {
+//   var arr = friends.map(el => `${this.name} is friends with ${el}`);
+//   console.log(arr);
+// }
+// var friends = ['bob', 'jane', 'mark'];
+// new Person('John').myFriends(friends);
+
+// es5
+// var john = ['john', 26];
+// var name = john[0];
+// var age = john[1];
+
+// es6
+const [name, age] = ['john', 26]
+// for objext
+const {firstName, lastName} = obj;
