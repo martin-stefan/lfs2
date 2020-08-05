081dfdc0a6984cafcbfb28d3d106a1e994ea256e
martin-stefan <55285748+martin-stefan@users.noreply.github.com>
2020-08-05 11:44:07 -0400

    lol

--- complete-javascript-course-master/7-ES6/starter/script.js
+++ complete-javascript-course-master/7-ES6/starter/script.js
@@ -22,9 +22,59 @@
 
 // Blocks and IFFes in es6
 // creates a private block
-{
-  const a = 1;
-  let b = 2;
-  var c = 3; // var will not be private in this block
+// {
+//   const a = 1;
+//   let b = 2;
+//   var c = 3; // var will not be private in this block
 
-}
+// }
+
+// let first = 'john';
+// let last = 'smith';
+// const yob = 1990;
+
+// function calc(year) {
+//   return 2016 - year;
+// }
+
+// // es5
+// console.log(first + " " + last + " is " + calc(yob) );
+
+// // es6
+// // template literals
+// console.log(`${first} ${last} is ${calc(yob)}`)
+
+
+// const n = `${first} ${last}`;
+// console.log(n.startsWith('j'));
+// console.log(n.endsWith('h'));
+// console.log(n.includes(' '));
+
+// console.log(first.repeat(5));
+// console.log(`${first} `.repeat(5));
+
+// es6 arrow functions
+
+const years = [1990, 1999, 1989];
+
+// es5
+// var ages = years.map(function(el) {
+//   return 2016 - el;
+// });
+
+
+// es6
+const ages = years.map(el => 2016 - el);
+console.log(ages);
+
+
+let ages2 = years.map((el, index) => `Age of ${index + 1}: ${2016 - el}`);
+console.log(ages2);
+
+ages2 = years.map((el, index) => {
+  const now = new Date().getFullYear();
+  const age = now - el;
+  return `Age of ${index + 1}: ${age}`
+})
+
+console.log(ages2)
