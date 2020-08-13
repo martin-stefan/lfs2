6b0204d1be379a722bc725830aaab1db94414a0c
martin-stefan <55285748+martin-stefan@users.noreply.github.com>
2020-08-13 18:12:26 -0400

    there is a bug in my code

--- complete-javascript-course-master/9-forkify/starter/src/js/index.js
+++ complete-javascript-course-master/9-forkify/starter/src/js/index.js
@@ -1,7 +1,28 @@
 // Global app controller
 
 import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements } from './views/base';
 
-const search = new Search('pizza');
-console.log(search);
-search.getResults();
+
+
+const state = {};
+
+const controlSearch = async () => {
+  const query = searchView.getInput();
+
+  if (query) {
+    state.search = new Search(query);
+
+    
+    await state.search.getResults();
+
+    console.log('here ----> ' + state.search.results);
+    searchView.renderResults(state.search.results);
+  }
+}
+
+elements.searchForm.addEventListener('submit', e => {
+  e.preventDefault();
+  controlSearch();
+});
