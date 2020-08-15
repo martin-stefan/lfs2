53680ff2d5f25fcf323c505182c52021c7c01284
Stefan Martin <stefanmartin410@gmail.com>
2020-08-15 19:30:02 -0400

    some work

--- complete-javascript-course-master/9-forkify/starter/src/js/views/searchView.js
+++ complete-javascript-course-master/9-forkify/starter/src/js/views/searchView.js
@@ -9,6 +9,21 @@ export const clearResults = () => {
   elements.searchResList.innerHTML = '';
 }
 
+const limitTitle = (title, limit = 17) => {
+  if (title.length > limit) {
+    const newTitle = []
+    title.split(' ').reduce((acc, cur) => {
+      if (acc + cur.length <= limit) {
+        newTitle.push(cur);
+      }
+      return acc + cur.length
+    }, 0);
+
+    return `${newTitle.join(' ')}...`
+  }
+  return title;
+};
+
 const renderRecipe = recipe => {
   const markup = `
     <li>
@@ -17,7 +32,7 @@ const renderRecipe = recipe => {
               <img src="${recipe.image_url}" alt="${recipe.title}">
           </figure>
           <div class="results__data">
-              <h4 class="results__name">${recipe.title}</h4>
+              <h4 class="results__name">${limitTitle(recipe.title)}</h4>
               <p class="results__author">${recipe.publisher}</p>
           </div>
       </a>
