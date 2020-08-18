f2bd4be6f8e56c296980374d7aee270f1aac8522
martin-stefan <55285748+martin-stefan@users.noreply.github.com>
2020-08-18 10:31:11 -0400

    some work

--- complete-javascript-course-master/9-forkify/starter/src/js/views/searchView.js
+++ complete-javascript-course-master/9-forkify/starter/src/js/views/searchView.js
@@ -8,7 +8,15 @@ export const clearInput = () => {
 export const clearResults = () => {
   elements.searchResList.innerHTML = '';
   elements.searchResPages.innerHTML = '';
-}
+};
+
+export const highlightSelected = id => {
+  const resultsArr = Array.from(document.querySelectorAll('.results__link'));
+  resultsArr.forEach(el => {
+    el.classList.remove('results__link--active');
+  });
+  document.querySelector(`a[href*="#${id}"]`).classList.add('results__link--active');
+};
 
 const limitTitle = (title, limit = 17) => {
   if (title.length > limit) {
