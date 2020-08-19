a1c29530df0ce460ab89bdcc9ec0faab6f480104
martin-stefan <55285748+martin-stefan@users.noreply.github.com>
2020-08-19 00:12:58 -0400

    finishing

--- complete-javascript-course-master/9-forkify/starter/src/js/models/Likes.js
+++ complete-javascript-course-master/9-forkify/starter/src/js/models/Likes.js
@@ -6,12 +6,18 @@ export default class Likes {
   addLike(id, title, author, img) {
     const like = { id, title, author, img };
     this.likes.push(like);
+
+    // local storage
+    this.persistData();
     return like;
   }
 
   deleteLike(id) {
     const index = this.likes.findIndex(el => el.id === id);
     this.likes.splice(index, 1);
+
+    // local sotrage
+    this.persistData();
   }
 
   isLiked(id) {
@@ -20,6 +26,17 @@ export default class Likes {
 
   getNumLikes() {
     return this.likes.length;
+  }
+
+  persistData() {
+    localStorage.setItem('likes', JSON.stringify(this.likes));
+  }
+
+  readStorage() {
+    const storage = JSON.parse(localStorage.getItem('likes'));
+
+    // restores from local storage
+    if (storage) this.likes = storage;
   }
 
 }
